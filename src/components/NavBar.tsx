39ade06395f6a0c2db475e782a7fef28933c171a
dhanushd-27 <dhanushsai2005@gmail.com>
2025-03-05 12:22:41 +0530

    Update: Twitter Link to Twitter Follow Link

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,7 @@ export default function NavBar() {
     <div className='flex gap-7 px-4 py-2'>
       <Link href={'/'} className='nav-option'>Home</Link>
       <Link href={'/projects'} className='nav-option'>Projects</Link>
-      <Link href={'https://x.com/orca_x27'} className='nav-option' target='_blank'>Twitter</Link>
+      <Link href={'https://x.com/intent/follow?screen_name=orca_x27'} className='nav-option' target='_blank'>Twitter</Link>
     </div>
   )
 }
