d83c5016d5a387cbe842ef4ea80cc6d7183f71c8
dhanushd-27 <dhanushsai2005@gmail.com>
2025-05-21 07:35:22 +0530

    Add Tailwind Vars and Classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+    <div className="bg-bg-dark h-screen w-screen flex justify-center items-center">
+      <div className="box">
+        <p className='text-default'>Hello world</p>
+      </div>
+    </div>
+    </>
   )
 }
