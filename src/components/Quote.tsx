d7e375a801de29450f7d2c00edb5820c0ad4f7f2
dhanushd-27 <dhanushsai2005@gmail.com>
2025-05-22 19:36:28 +0530

    Update: Quote Loader

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -10,16 +10,26 @@ type Quote = {
 
 export default function Quote() {
   const [quote, setQuote] = useState<Quote>()
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    getRandomQuote().then(setQuote)
-  }, [])
+    getRandomQuote().then(setQuote).then(() => setIsLoading(false));
 
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className='box col-start-4 col-end-10 row-start-11 row-end-13 overflow-auto gap-1 flex items-center justify-center'>
+        <div className='w-8 h-8 rounded-full border-4 border- border-t-transparent animate-spin border-accent-cursor'>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <div className='box col-start-4 col-end-10 row-start-11 row-end-13 overflow-auto gap-1 group hover:cursor-pointer'>
-          <p className='text-default text-start group-hover:blur-none blur-xs transition-all duration-300'>{quote?.quote}</p>
-          <p className='text-default text-end group-hover:blur-none blur-xs transition-all duration-300'>- {quote?.author}</p>
+        <p className='text-default text-start group-hover:blur-none blur-xs transition-all duration-300'>{quote?.quote}</p>
+        <p className='text-default text-end group-hover:blur-none blur-xs transition-all duration-300'>- {quote?.author}</p>
     </div>
   )
 }
