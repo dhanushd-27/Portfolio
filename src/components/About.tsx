1ce1b2dfa278fa42cf000fa6186bd0a2929b5b8e
dhanushd-27 <dhanushsai2005@gmail.com>
2025-05-24 11:25:19 +0530

    Add About Section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div className='box col-start-6 col-end-10 row-start-1 row-end-7'>
+    <div className='box col-start-6 col-end-10 row-start-1 row-end-7 flex flex-col gap-6'>
       <p className='header'>About</p>
+      <div className='text-text-default tracking-wide text-sm leading-6 text-justify px-4 flex flex-col gap-1'>
+        <p className=''>
+          I&apos;m a 21-year-old Computer Science graduate from Sai Vidya Institute of Technology, Bengaluru (class of 2025), with a strong interest in <span className='text-accent-white underline'>Web development and DevOps</span>.
+        </p>
+        <p className=''>
+          I&apos;m currently <span className='text-accent-white underline'>open to internship or full-time opportunities</span> in web development or DevOps.
+        </p>
+        <p className=''>
+          I&apos;m currently exploring vertical scaling, containerization, and kubernetes to deepen my backend and DevOps knowledge.
+        </p>
+        <p className=''>
+          Outside of tech, I enjoy watching anime and cricket, playing FPS games like Valorant, and reading fiction. A book I recommend is A Man Called Ove by Fredrik Backman.
+        </p>
+        <p className=''>
+          You can find my projects and contributions on my <a href="https://github.com/dhanushd-27" className="text-accent-white hover:underline">GitHub</a>.
+        </p>
+      </div>
     </div>
   )
 }
