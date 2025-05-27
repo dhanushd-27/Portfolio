688308970e7429c42d933da5dbaf91c09f977376
dhanushd-27 <dhanushsai2005@gmail.com>
2025-05-27 14:00:54 +0530

    Update Profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -5,12 +5,13 @@ import TextDetails from './TextDetails'
 
 export default function Profile() {
   return (
-    <div className='box col-start-4 col-end-6 row-start-1 row-end-7 flex items-center justify-center hide-scrollbar overflow-y-auto'>
-      <div className='py-4 w-full flex flex-col gap-4 items-center justify-start'>
+    <div className='box col-start-4 col-end-6 row-start-1 row-end-7 flex items-center justify-center hide-scrollbar overflow-y-auto relative py-4'>
+      <div className='py-4 w-full h-11/12 flex flex-col gap-4 items-center justify-start'>
         <AvailableForWork />
         <Image src={'/profile-pfp.jpg'} alt='Profile Picture' height={200} width={200} className='rounded-lg'/>
         <TextDetails />
       </div>
+      <div className='pointer-events-none absolute bottom-0 left-0 w-full h-15 bg-gradient-to-b from-transparent to-bg-card'></div>
     </div>
   )
 }
