89cd60d2c630d6684bb53303b6e8f261b400524b
dhanushd-27 <dhanushsai2005@gmail.com>
2025-05-30 21:29:42 +0530

    Update Switch'

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -1,10 +1,12 @@
+'use client'
+
 import React from 'react'
 
 export default function Switch() {
   return (
     <div className='hidden lg:flex box row-start-9 row-end-11 col-start-4 col-end-6 justify-center items-center'>
-      <div className='w-full flex p-3 box items-center justify-center'>
-        <p className='text-sm text-text-muted'>Switch</p>
+      <div onClick={() => {}} className='w-full flex p-3 box items-center justify-center bg-surface-primary hover:bg-surface-secondary transition-all duration-300 ease-in-out transform hover:scale-95'>
+        <p className='text-sm text-default'>Switch</p>
       </div>
     </div>
   )
