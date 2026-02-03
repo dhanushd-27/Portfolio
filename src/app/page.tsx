5351822095c4884cef8cce99b4a66e34d4610aa0
dhanushd-27 <dhanushsai2005@gmail.com>
2026-02-03 20:39:29 +0530

    Create new Next app using Bun and add tailwind css variables to it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,9 @@
-import Quote from "@/components/Quote"
-import Techstack from "@/components/TechStack/Techstack"
-import Profile from "@/components/Profile/Profile"
-import Links from "@/components/Socials/Socials"
-import About from "@/components/About"
-import Photos from "@/components/Photos"
-import Projects from "@/components/Projects/Projects"
-import CopyMail from "@/components/CopyMail"
-import Switch from "@/components/Switch"
-import React from "react"
+import Image from "next/image";
 
 export default function Home() {
-
   return (
-    <>
-      <div className="bg-bg-dark flex lg:h-screen justify-center items-center p-5">
-        <div className='screen'>
-          <Profile />
-          <About />
-          <Techstack />
-          <Projects />
-          <CopyMail />
-          <Links />
-          <Switch />
-          <Photos />
-          <Quote />
-        </div>
-      </div>
-    </>
-  )
+    <div>
+      Hello World
+    </div>
+  );
 }
